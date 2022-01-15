ad7813ff582c8fdc5650e0c998a74e52ce2de987
abbas ali <abbas.ali5788@gmail.com>
2022-01-15 16:57:47 +0500

    initial commit

---
+++ src/constants/images.js
@@ -0,0 +1,6 @@
+// const ripple = require('../assets/images/ripple.png');
+// import all images as above and export them using below format
+
+// export default {
+//   // ripple,image2,img3,.....
+// };
